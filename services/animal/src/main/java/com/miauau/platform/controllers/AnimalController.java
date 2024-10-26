6a84b14ebbe36d2caaa5baec757dc02e95f51356
Michel Brito <michel.brito@inspira.com.br>
2024-10-26 13:18:25 -0300

    minor

--- services/animal/src/main/java/com/miauau/platform/controllers/AnimalController.java
+++ services/animal/src/main/java/com/miauau/platform/controllers/AnimalController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/animal")
+@RequestMapping("/api/v1/animals")
 @RequiredArgsConstructor
 public class AnimalController {
 
